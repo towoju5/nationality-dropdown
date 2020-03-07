3561185e9a1bf15631b03bb4eeb8d3c608f6c190
towoju5 <40261626+towoju5@users.noreply.github.com>
2020-03-07 02:24:19 +0100

    Create nigeria states.php

---
+++ nigeria states.php
@@ -0,0 +1,39 @@
+<select name="states_of_nigeria" id="select">
+    <option>ABIA</option>
+    <option>ABUJA FCT</option>
+    <option>ADAMAWA</option>
+    <option>AKWA IBOM</option>
+    <option>ANAMBRA</option>
+    <option>BAUCHI</option>
+    <option>BAYELSA</option>
+    <option>BENUE</option>
+    <option>BORNO</option>
+    <option>CROSS RIVER</option>
+    <option>DELTA</option>
+    <option>EBONYI</option>
+    <option>EDO</option>
+    <option>EKITI</option>
+    <option>ENUGU</option>
+    <option>GOMBE</option>
+    <option>IMO</option>
+    <option>JIGAWA</option>
+    <option>KADUNA</option>
+    <option>KANO</option>
+    <option>KATSINA</option>
+    <option>KEBBI</option>
+    <option>KOGI</option>
+    <option>KWARA</option>
+    <option>LAGOS</option>
+    <option>NASSARAWA</option>
+    <option>NIGER</option>
+    <option>OGUN</option>
+    <option>ONDO</option>
+    <option>OSUN</option>
+    <option>OYO</option>
+    <option>PLATEAU</option>
+    <option>RIVERS</option>
+    <option>SOKOTO</option>
+    <option>TARABA</option>
+    <option>YOBE</option>
+    <option>ZAMFARA</option>
+</select>
